8963e457a08ac4a1f506639e3544bede65810601
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2022-12-02 17:04:33 -0600

    add some vectors tests

--- tests/vector/vector_tests.cpp
+++ tests/vector/vector_tests.cpp
@@ -6,11 +6,314 @@
 /*   By: cjulienn <cjulienn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:42:21 by cjulienn          #+#    #+#             */
-/*   Updated: 2022/12/01 16:24:55 by cjulienn         ###   ########.fr       */
+/*   Updated: 2022/12/02 16:54:32 by cjulienn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/test_includes.hpp"
+
+/* tests all constructors and operator = */
+static void	test_constructors(Colors &col)
+{
+	// TO IMPLEMENT
+}
+
+/* test all the insert versions */
+static void test_insertion(Colors &col) // WIP
+{
+	std::vector<int> 				test;
+	std::vector<int>::iterator		it = test.begin();
+
+	std::cout << "Creating an empty int vector..." << std::endl;
+	std::cout << "Insert a single value" << std::endl;
+
+	std::cout << "Insert int of value 4 in vector..." << std::endl;
+	test.insert(it, 4);
+}
+
+/* pop_back test battery 
+!!! if used on empty container it causes undefined behaviour !!! */
+static void test_pop_back(Colors &col) // to test
+{
+	separator(col);
+	std::cout << col.bdYellow() << "Testing pop_back method..." << col.reset()
+	<< std::endl << std::endl;
+	separator(col);
+
+	std::vector<int>		test;
+
+	std::cout << "Inserting 5 values to an empty vector" << std::endl;
+	
+	test.push_back(5);
+	test.push_back(76);
+	test.push_back(-4567);
+	test.push_back(0);
+	test.push_back(-1);
+
+	/* printing vector content (without using pop back) */
+	std::cout << "printing vector..." << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "Index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "size of vector is " << test.size() << std::endl;
+	std::cout << "capacity of vector is : " << test.capacity() << std::endl;
+	
+	/* decrease the vector size one by one using pop back and checks at every deletion
+	the relevant infos */
+	
+	// first deletion
+	std::cout << "trying pop_back method" << std::endl;
+	std::cout << "erase one value" << std::endl;
+	test.pop_back();
+	std::cout << "printing updated vector content" << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "Index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "size of the updated vector is : " << test.size() << std::endl;
+	std::cout << "capacity of the updated vector is : " << test.capacity() << std::endl << std::endl;
+	
+	// second deletion
+	std::cout << "erase one value" << std::endl;
+	test.pop_back();
+	std::cout << "printing updated vector content" << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "Index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "size of the updated vector is : " << test.size() << std::endl;
+	std::cout << "capacity of the updated vector is : " << test.capacity() << std::endl << std::endl;
+
+	// third deletion
+	std::cout << "erase one value" << std::endl;
+	test.pop_back();
+	std::cout << "printing updated vector content" << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "Index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "size of the updated vector is : " << test.size() << std::endl;
+	std::cout << "capacity of the updated vector is : " << test.capacity() << std::endl << std::endl;
+
+	// fourth deletion
+	std::cout << "erase one value" << std::endl;
+	test.pop_back();
+	std::cout << "printing updated vector content" << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "Index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "size of the updated vector is : " << test.size() << std::endl;
+	std::cout << "capacity of the updated vector is : " << test.capacity() << std::endl << std::endl;
+
+	// fifth deletion
+	std::cout << "erase one value" << std::endl;
+	test.pop_back();
+	std::cout << "printing updated vector content" << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "Index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "size of the updated vector is : " << test.size() << std::endl;
+	std::cout << "capacity of the updated vector is : " << test.capacity() << std::endl << std::endl;
+	// PD : not trying a sixth deletion because vector is empty (would cause undefined behaviour)
+	
+	separator(col);
+	std::cout << std::endl << col.bdYellow() << "pop_back tests over" << col.reset() << std::endl;
+	separator(col);
+}
+
+/* test the capacity methods of vector. Contains : 
+=> size
+=> max_size
+=> resize
+=> capacity
+=> empty
+=> reserve */
+static void	test_capacity_methods(Colors &col)
+{
+	separator(col);
+	std::cout << col.bdYellow() << "Testing capacity methods..." << col.reset()
+	<< std::endl << std::endl;
+	separator(col);
+
+	std::vector<std::string>		test;
+	
+	std::cout << "Creating an empty array" << std::endl;
+	std::cout << "size should be equal to 0 and empty to false..." << std::endl;
+	std::cout << "test.size = " << test.size() << " and test.empty = " << std::boolalpha << test.empty() << std::endl;
+
+	std::cout << "pushing back some values to test..." << std::endl;
+	
+	test.push_back("salut");
+	test.push_back("");
+	test.push_back("je");
+	test.push_back("s appelle");
+	test.push_back("Groot");
+
+	std::cout << "test.empty = " << std::boolalpha << test.empty() << " and test.size = " << test.size() << std::endl;
+	
+	/* MAX SIZE */
+	std::cout << "Testing max_size method..." << std::endl;
+	std::cout << "test.max_size = " << test.max_size() << std::endl;
+
+	/* RESIZE */
+	std::cout << "Testing resize method" << std::endl;
+	
+
+	
+
+	/* CAPACITY */
+	std::cout << "Testing capacity method..." << std::endl;
+	std::cout << "test.capacity = " << test.capacity() << std::endl;
+
+	/* insert some values to check the changes in capacity */
+	std::cout << "Checking changes in capacity related to changes in size..." << std::endl; // to improve
+	test.push_back("Check"); 
+	test.push_back("changes ");
+	test.push_back("in");
+	test.push_back("vector");
+	test.push_back("capcity");
+	
+	std::cout << "test.capacity = " << test.capacity() << std::endl;
+
+	separator(col);
+	std::cout << std::endl << col.bdYellow() << "capacity tests over" << col.reset() << std::endl;
+	separator(col);
+}
+
+/* testing the push back methods of vector */
+static void	test_push_back(Colors &col) // to test
+{
+	separator(col);
+	std::cout << col.bdYellow() << "Testing push_back method..." << col.reset()
+	<< std::endl << std::endl;
+	separator(col);
+	
+	std::vector<int>			test;
+	
+	std::cout << "Testing push back" << std::endl;
+	
+	std::cout << "test.empty = " << std::boolalpha << test.empty() << std::endl;
+	std::cout << "test.size = " << test.size() << std::endl;
+	std::cout << "test.capacity = " << test.capacity() << std::endl;
+	std::cout << "Inserting 5 values one by one using push_back" << std::endl;
+	test.push_back(5);
+	test.push_back(76);
+	test.push_back(-4567);
+	test.push_back(0);
+	test.push_back(-1);
+	std::cout << "test.empty = " << std::boolalpha << test.empty() << std::endl;
+	std::cout << "test.size = " << test.size() << std::endl;
+	std::cout << "Printing the 5 values" << std::endl;
+	for (std::size_t i = 0; i < test.size(); i++)
+		std::cout << "index [" << i << "] = " << test[i] << std::endl;
+	std::cout << "test.capacity = " << test.capacity() << std::endl;
+
+	separator(col);
+	std::cout << std::endl << col.bdYellow() << "Push_back tests over" << col.reset() << std::endl;
+	separator(col);
+}
+
+static void	test_access_elems(Colors &col) // to test
+{
+	separator(col);
+	std::cout << col.bdYellow() << "Testing methods to access elements in the vector..." << col.reset()
+	<< std::endl << std::endl;
+	separator(col);
+
+	std::vector<int>			test;
+	
+	test.push_back(5);
+	test.push_back(76);
+	test.push_back(-4567);
+	test.push_back(0);
+	test.push_back(-1);
+
+	std::cout << "A test vector with 5 values has been created [5, 76, -4567, 0, -1]" << std::endl;
+	std::cout << "Verifying the behaviour of operator []" << std::endl;
+	for (std::size_t i = 0; i < test.size(); i++)
+		std::cout << "test[" << i << "] = " << test[i] << std::endl;
+	
+	std::cout << "Testing test.at with valid indexes" << std::endl;
+	for (std::size_t i = 0; i < test.size(); i++)
+		std::cout << "test.at(" << i << ") = " << test.at(i) << std::endl;
+	std::cout << "Testing test.a with invalid index" << std::endl;
+		std::cout << test.at(test.size()) << std::endl;
+
+	/* data is C+11 BUT has been declared C++98 retroactively for conveniance */
+	std::cout << "Testing with test.data" << std::endl;
+	
+	int	*ptn = test.data();
+	
+	std::cout << "iterating with an int pointer (should produce the same result as iterating with " 
+	<< "a for loop and at or [])" << std::endl;
+	for (std::size_t i = 0;  i < test.size(); i++) // to test
+	{
+		std::cout << *ptn << std::endl;
+		ptn++;
+	}
+	
+	/* front and back on empty vector got undefined behaviour !!! */
+	std::cout << "Testing test.front and test.back" << std::endl;
+	std::cout << "test.front() = " << test.front() << std::endl;
+	std::cout << "test.back() = " << test.back() << std::endl;
+
+	std::cout << "Testing front and back with another vector with one value" << std::endl;
+	
+	std::vector<int>		test_2;
+
+	test_2.push_back(42);
+	std::cout << "front and back should have the same value (42)" << std::endl;
+	std::cout << "test_2.front() = " << test_2.front() << " and test_2.back = " << test_2.back() << std::endl;
+		
+	separator(col);
+	std::cout << std::endl << col.bdYellow() << "Access tests over" << col.reset() << std::endl;
+	separator(col);
+}
+
+/* testing the method assign of vector */
+static void	test_assign(Colors &col)
+{
+	separator(col);
+	std::cout << col.bdYellow() << "Testing assign method..." << col.reset()
+	<< std::endl << std::endl;
+	separator(col);
+
+	std::vector<int>		test;
+
+	test.push_back(5);
+	test.push_back(76);
+	test.push_back(-4567);
+	test.push_back(0);
+	test.push_back(-1);
+	
+	std::cout << "Creates a vector and push back 5 values..." << std::endl;
+	std::cout << "vector size = " << test.size() << std::endl;
+	std::cout << "vector capacity = " << test.capacity() << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "index [" << i << "] = " << test[i] << std::endl;
+	
+	/* assign with single value, without iterators */
+	std::cout << "replace content with assign" << std::endl;
+	test.assign(6, 42);
+	std::cout << "vector size = " << test.size() << std::endl;
+	std::cout << "vector capacity = " << test.capacity() << std::endl;
+	for (std::size_t i; i < test.size(); i++)
+		std::cout << "index [" << i << "] = " << test[i] << std::endl;
+	
+	/* assign with iterators */
+	std::vector<int>					other;
+	std::vector<int>::iterator			itb = other.begin();
+	std::vector<int>::iterator			ite = other.end();
+
+	other.push_back(21);
+	other.push_back(42);
+	other.push_back(72);
+
+	std::cout << "replace content with a range of iterators" << std::endl;
+	test.assign(itb, ite); // should work, but test
+	
+	
+	
+	separator(col);
+	std::cout << std::endl << col.bdYellow() << "Assign tests over" << col.reset() << std::endl;
+	separator(col);
+}
+
+
+
+
+
 
 /* all the tests to assess the viability of ft::vector */
 void	vector_verif_procedure(Colors &col)
@@ -246,12 +549,6 @@ void	vector_verif_procedure(Colors &col)
 	std::cout << "size of ft_vector = " << ft_vector.size() << std::endl;
 	separator(col);
 
-	/* TEST OF GET ALLOCATOR */ 
-	/* checking the behaviour of get_allocator*/
-	std::cout << "check the get_allocator method" << std::endl; // << not overloaded
-	std::cout << "get_allocator method of stl_vector = " << stl_vect.get_allocator() << std::endl;
-	std::cout << "get_allocator method of ft_vector = " << ft_vector.get_allocator() << std::endl;
-	
 	separator(col);
 	//---------------------------------------------------------------------------------------//
 	std::cout << col.bdYellow() << "-----------------------------" << col.reset() << std::endl;
